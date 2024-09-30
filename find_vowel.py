1dd2ee929287fa0726915979d20806c1665f262c
cuunlit615 <114757891+cuunlit615@users.noreply.github.com>
2024-10-01 00:55:29 +0800

    Update find_vowel.py
    add comment

--- find_vowel.py
+++ find_vowel.py
@@ -1,11 +1,10 @@
-
 def solution(s):
-    s = s.lower()
-    num = []
+    s = s.lower()#全部轉小寫
+    num = []#定義位置列表
     vowel = ['a' ,'e' ,'i' ,'o' ,'u']
     for v in vowel:
-        while v in s:
-            num.append(s.find(v))
-            s = s.replace(v ,'t' ,1)
-    num.sort()
+        while v in s:#當S含有當次搜尋之母音
+            num.append(s.find(v))#將該母音位置加入nums
+            s = s.replace(v ,'，' ,1)#將該母音以'，'替代
+    num.sort()#排序
     return num
